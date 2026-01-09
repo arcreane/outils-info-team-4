84a054b348a8fc27de4b121a4163bd63718c5db8
Antonin900 <antonin.robert0792@gmail.com>
2026-01-09 11:49:51 +0100

    Update projectiles.py

--- projectiles.py
+++ projectiles.py
@@ -98,7 +98,20 @@ while running:
     for enemy in enemies:
         enemy.draw(screen)
 
+class Enemy:
+    def __init__(self, x, y):
+        # Charger l'image du sprite ennemi
+        self.image = pygame.image.load('smilie_cookie.png').convert_alpha()
+
+        # Redimensionner le sprite (ajustez la taille selon vos besoins)
+        self.image = pygame.transform.scale(self.image, (50, 50))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 
     pygame.display.flip()
 
 pygame.quit()
+
